519b821c05c487aa474841a6d2b99f11f72b2a54
2K-d <rouxv894@gmail.com>
2022-12-14 15:46:34 +0100

    ajout de la requête pour chercher les scores, renvoie vide

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -11,6 +11,10 @@ def hello_world():
     x = 'world'
     return f"Hello, {x}"
 
+@app.route('/quiz-info', methods=['GET'])
+def GetQuizInfo():
+	return {"size": 0, "scores": []}, 200
+    
 @app.route('/login', methods=['POST'])
 def login():
     payload = request.get_json()
