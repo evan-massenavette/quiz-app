26d5cb4859d7833a5fd75e9684eb218e569553ed
Evan <evan.massenavette@outlook.fr>
2022-12-16 16:39:19 +0100

    Finished Question and Answer model classes

--- quiz-api/models.py
+++ quiz-api/models.py
@@ -1,57 +1,39 @@
 import json
 
 
-class JSONable():
-    def __init__(self, json_obj):
-        self.json_obj = json_obj
-
+class JSONable(dict):
     def to_json(self):
-        return json.dumps(self, cls=JSONableEncoder)
+        return json.dumps(self)
 
-    def __getitem__(self, key):
-        return self.json_obj[key]
+    def __getattr__(self, name: str):
+        return self[name]
 
-    def __setitem__(self, key, value):
-        return self.json_obj[key] = value
+    def __setattr__(self, name: str, value):
+        self[name] = value
 
 
 class Answer(JSONable):
     def __init__(self, text: str, isCorrect: bool):
-        super.__init__({
-            'text': text,
-            'isCorrect': isCorrect
-        })
+        self.text = text
+        self.isCorrect = isCorrect
 
     def __init__(self, json_obj: dict[str]):
-        super().__init__(json_obj)
+        self.text = json_obj['text']
+        self.isCorrect = json_obj['isCorrect']
 
 
 class Question(JSONable):
-    def __init__(self, text: str, title: str, image: str, position: int, possibleAnswers: list[Answer]):
-        super().__init__({
-            "title": title,
-            "text": text,
-            "position": position,
-            "image": image,
-            "possibleAnswers": possibleAnswers
-        })
+    def __init__(self, title: str, text: str, image: str, position: int, possibleAnswers: list[Answer]):
+        self.title = title
+        self.text = title
+        self.image = image
+        self.position = position
         self.possibleAnswers = possibleAnswers
 
-    def __init__(self, json: dict[str]):
-        super().__init__({
-            "title": json["title"],
-            "text": json["text"],
-            "position": json["position"],
-            "image": json["image"],
-            "possibleAnswers": list(map(lambda x: Answer(x), json["possibleAnswers"]))
-        })
+    def __init__(self, json_obj: dict[str]):
+        self.title = json_obj['title']
+        self.text = json_obj['title']
+        self.image = json_obj['image']
+        self.position = json_obj['position']
         self.possibleAnswers = list(
-            map(lambda x: Answer(x), json["possibleAnswers"]))
-
-
-class JSONableEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, JSONable):
-            return obj.json_obj
-        else:
-            return json.JSONEncoder.default(self, obj)
+            map(lambda x: Answer(x), json_obj["possibleAnswers"]))
