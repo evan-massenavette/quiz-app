20d69984168e60ab16c98123d2934f51add3bd2a
Evan <evan.massenavette@outlook.fr>
2022-12-16 16:39:41 +0100

    Add a few unimplemented end points

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -31,17 +31,72 @@ def login():
         return 'Unauthorized', 401
 
 
+@app.route('/questions/all', methods=['DELETE'])
+def delete_all_question():
+    # Check for admin auth
+    # request.headers.get('Authorization')
+
+    # Delete all questions from database
+
+    return 'Not implemented', 501
+
+
+@app.route('/questions', methods=['GET'])
+def get_question():
+    # Check if the position arg was given
+    if (not request.args['position']):
+        return 'Question position must be given', 422
+
+    # Get request question or database<
+
+    return 'Not implemented', 501
+
+
+@app.route('/questions', methods=['PUT'])
+def update_question():
+    # Check for admin auth
+    # request.headers.get('Authorization')
+
+    # Read question in request
+    try:
+        payload = request.get_json()
+        question = Question(payload)
+    except Exception as e:
+        return f'Cannot read question: {e}', 400
+
+    # Update question at given postion in database
+
+    return 'Not implemented', 501
+
+
 @app.route('/questions', methods=['POST'])
 def add_question():
-    request.headers.get('Authorization')
-    payload = request.get_json()
-    mama = Question(payload)
+    # Check for admin auth
+    # request.headers.get('Authorization')
+
+    # Read question in request
+    try:
+        payload = request.get_json()
+        question = Question(payload)
+    except Exception as e:
+        return f'Cannot read question: {e}', 400
+
+    # Check that we can add the question (there is not question already at that position)
+    can_add_question = True
+    if (can_add_question):
+        # Add question to database
+        pass
+    else:
+        return f'Question already present at position {question.position}', 422
+
+    # Add question to database
+
     print()
-    print(mama.to_json())
-    print(mama["possibleAnswers"].to_json())
+    print(question)
+    # print(mama.possibleAnswers[0].to_json())
     print()
-    return 'aze', 200
+    return 'Not implemented', 501
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
