e0d0b81e1797f9a410b70e8551f5c809f266d0a6
2K-d <rouxv894@gmail.com>
2022-12-16 16:57:43 +0100

    ux question pas fini

---
+++ quiz-ui/src/views/QuestionDisplay.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>{{question.questionTitle}}</h1>
+    <p>{{question.questionText}}</p>
+    <img v-if="question.image" :src="question.image" />
+    <!--a v-for="(answer,index) in question.possibleAnswers" :key="answer" @click="$emit('answer-selected', index)">{{answer.text}}</a-->
+</template>
+<script>
+export default{
+    props: {
+        question: {
+            type: Object
+        }
+    },
+    emits: ["answer-selected"]
+}
+</script>
---
+++ quiz-ui/src/views/QuestionManager.vue
@@ -0,0 +1,54 @@
+<template>
+<h1>Question {{ currentQuestionPosition }} / {{ totalNumberOfQuestion }}</h1>
+<QuestionDisplay :question="currentQuestion" @click-on-answer="answerClickedHandler" />  
+</template>
+<script>
+import QuizApiService from '@/services/QuizApiService';
+import QuestionDisplay from './QuestionDisplay.vue';
+export default{
+  components: {
+    QuestionDisplay
+  },
+  async created(){
+    this.totalNumberOfQuestion=await QuizApiService.getQuizInfo().data.size;
+    if(this.totalNumberOfQuestion>0){
+      this.loadQuestionByPosition();
+    } else {
+      this.endQuiz();
+    }
+  },
+  data(){
+    return {
+      currentQuestion:{
+        questionTitle:"",
+        questionText:"",
+        possibleAnswers:[],
+        image:""
+      },
+      currentQuestionPosition:1,
+      totalNumberOfQuestion:0
+    };
+  },
+  methods:{
+    async answerClickedHandler(){
+      //TODO ajouter de la vérification du résultat depuis le signal émis
+      this.currentQuestionPosition++;
+      if(this.currentQuestionPosition>this.totalNumberOfQuestion){
+        this.endQuiz();
+      } else {
+        this.loadQuestionByPosition()
+      }
+    },
+    endQuiz(){
+      //TODO Ajouter fin quizz
+    },
+    async loadQuestionByPosition(){
+      question=await QuizApiService.getQuestion(this.currentQuestionPosition).data
+      this.currentQuestion.questionText=question.text;
+      this.currentQuestion.questionTitle=question.title;
+      this.currentQuestion.image=question.image;
+      this.currentQuestion.possibleAnswers=question.possibleAnswers;
+    }
+  }
+}
+</script>
