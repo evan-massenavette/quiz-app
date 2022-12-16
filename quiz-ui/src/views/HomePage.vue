1ccac330f58cd864ee010aacff7d91406fc55995
Evan <evan.massenavette@outlook.fr>
2022-12-16 14:58:11 +0100

    HomePage component

---
+++ quiz-ui/src/views/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Home page</h1>
+
+  <div v-for="scoreEntry in registeredScores" v-bind:key="scoreEntry.date">
+    {{ scoreEntry.playerName }} - {{ scoreEntry.score }}
+  </div>
+
+  <RouterLink to="/start-new-quiz-page">Démarrer le quiz !</RouterLink>
+
+</template>
+
+<script>
+import quizApiService from "@/services/QuizApiService";
+
+export default {
+  name: "HomePage",
+  data() {
+    return {
+      registeredScores: [],
+    };
+  },
+  async created() {
+    this.registeredScores = await quizApiService.getQuizInfo().data;
+    console.log("Composant Home page 'created'");
+  }
+};
+</script>
