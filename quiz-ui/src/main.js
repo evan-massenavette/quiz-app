ac5a42d558649d86146516d67f758d827423ab1a
Céline LEMONNIER <celine.lemonnier@edu.esiee.fr>
2023-01-03 16:53:49 +0100

    Modified main.js to import and use vuetify and mdi

--- quiz-ui/src/main.js
+++ quiz-ui/src/main.js
@@ -4,8 +4,32 @@ import router from './router'
 
 import './assets/main.css'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+import '@mdi/font/css/materialdesignicons.css'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 const app = createApp(App)
 
 app.use(router)
+app.use(vuetify)
+
+export default createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    }
+  },
+})
 
 app.mount('#app')
