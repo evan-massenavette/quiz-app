10d28c4f2f6cb207990c0fef8e1492f5ee7e06f6
Céline LEMONNIER <celine.lemonnier@edu.esiee.fr>
2023-01-04 20:10:00 +0100

    Modified HomePage content and style

--- quiz-ui/src/views/HomePage.vue
+++ quiz-ui/src/views/HomePage.vue
@@ -1,17 +1,22 @@
 <template>
-  <v-card>
-    <h1>Home page</h1>
-    <p>A little quiz about space and astronomy.</p>
+  <v-container>
+    <v-card>
+      <div class="card-content">
+        <h1>Home page</h1>
+        <div class="quiz-description">
+          <p>Here is a little quiz on space and astronomy.</p>
+        </div>
+        <RouterLink class="router-link-quiz" to="/start-new-quiz-page">Start the quiz now!</RouterLink>
 
-    <RouterLink to="/start-new-quiz-page">Start the quiz now!</RouterLink>
-
-    <div v-if="registeredScores && registeredScores.length>0">
-      <p>High scores</p>
-      <div v-for="scoreEntry in registeredScores" v-bind:key="scoreEntry.date">
-        {{ scoreEntry.playerName }} - {{ scoreEntry.score }}
+        <div v-if="registeredScores && registeredScores.length > 0">
+          <p>High scores</p>
+          <div v-for="scoreEntry in registeredScores" v-bind:key="scoreEntry.date">
+            {{ scoreEntry.playerName }} - {{ scoreEntry.score }}
+          </div>
+        </div>
       </div>
-    </div>
-  </v-card>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
@@ -29,3 +34,31 @@ export default {
   }
 };
 </script>
+
+<style>
+.card-content {
+  margin-left: 20px;
+  margin-right: 20px;
+  margin-top: 10px;
+  margin-bottom: 20px;
+}
+
+.quiz-description {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+.router-link-quiz {
+  display: inline-block;
+  width: 150px;
+  height: 30px;
+  font-size: large;
+  color: hsl(228, 87%, 52%);
+  align-items: center;
+  justify-content: center;
+}
+
+.router-link-quiz:hover {
+  background-color: white;
+}
+</style>
