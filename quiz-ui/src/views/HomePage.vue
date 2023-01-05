1428095f73017c67e5a9b5f1f16bc5d581eb6c27
Evan MASSENAVETTE <evan.massenavette@outlook.fr>
2023-01-05 17:35:36 +0100

    New homepage

--- quiz-ui/src/views/HomePage.vue
+++ quiz-ui/src/views/HomePage.vue
@@ -1,24 +1,17 @@
 <template>
-  <v-container>
-    <v-card class="mx-auto my-12">
-      <div class="card-content">
-        <h1>Home page</h1>
-        <div class="quiz-description">
-          <p>Here is a little quiz on space and astronomy.</p>
-        </div>
-        <RouterLink class="router-link-quiz" to="/start-new-quiz-page">Start the quiz now!</RouterLink>
-      </div>
-    </v-card>
-    <v-card class="mx-auto my-12" v-if="registeredScores && registeredScores.length > 0">
-      <div class="card-content">
+  <v-container id="main_card_wrapper">
+    <v-card id="main_card">
+      <h1>Space quiz !</h1>
+      <p>Here is a little quiz on space and astronomy.</p>
+      <v-btn id="goto_quiz_button" to="/start-new-quiz-page" ripple="false">
+        Start the quiz now!
+      </v-btn>
+      <v-container v-if="true || registeredScores && registeredScores.length > 0">
         <h1>High scores</h1>
-        <div class="quiz-description">
-          <p>Look at the best entries.</p>
-        </div>
         <div v-for="scoreEntry in registeredScores" v-bind:key="scoreEntry.date">
           {{ scoreEntry.playerName }} - {{ scoreEntry.score }}
         </div>
-      </div>
+      </v-container>
     </v-card>
   </v-container>
 </template>
@@ -31,6 +24,48 @@ export default {
   data() {
     return {
       registeredScores: [],
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+        },
+        {
+          name: 'Lollipop',
+          calories: 392,
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+        },
+      ]
     };
   },
   async created() {
@@ -39,30 +74,33 @@ export default {
 };
 </script>
 
-<style>
-.card-content {
-  margin-left: 20px;
-  margin-right: 20px;
-  margin-top: 10px;
-  margin-bottom: 20px;
+<style scoped>
+#main_card_wrapper {
+  width: 100%;
+  display: flex;
+  justify-content: center;
 }
 
-.quiz-description {
-  margin-top: 10px;
-  margin-bottom: 10px;
-}
-
-.router-link-quiz {
-  display: inline-block;
-  width: 150px;
-  height: 30px;
-  font-size: large;
-  color: hsl(228, 87%, 52%);
+#main_card {
+  padding: 10px 20px;
+  width: 800px;
+  display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 }
 
-.router-link-quiz:hover {
-  background-color: white;
+#main_card h1 {
+  text-align: center;
+}
+
+#main_card p {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+#goto_quiz_button {
+  background-color: rgb(var(--v-theme-accent));
+  width: fit-content;
 }
 </style>
