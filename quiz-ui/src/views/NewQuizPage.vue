8ea37100bfe212f8db8bdf09631d808f12bb4947
2K-d <rouxv894@gmail.com>
2023-01-06 23:01:37 +0100

    css NewQuizPage

--- quiz-ui/src/views/NewQuizPage.vue
+++ quiz-ui/src/views/NewQuizPage.vue
@@ -1,20 +1,24 @@
 <template>
-  <v-container>
-    <v-card class="mx-auto my-12">
-      <v-form v-model="form" @submit.prevent="launchNewQuiz">
+  <v-container id="main_card_wrapper" fluid>
+    <v-card id="main_card">
+      <v-form class="v-form" v-model="form" @submit.prevent="launchNewQuiz">
         <v-card-title>Enter your name:</v-card-title>
         <v-card-text>
           <v-text-field prepend-icon="mdi-form-textbox" type="text" label="Name" clearable placeholder="Enter your name"
             :rules="rules" :readonly="loading" v-model="username" />
         </v-card-text>
         <v-card-actions>
-          <v-btn id="go_button" :disabled="!form" :loading="loading" type="submit">GO!</v-btn>
+          <v-btn id="go_button" :disabled="!form" :loading="loading" type="submit" block>GO!</v-btn>
         </v-card-actions>
       </v-form>
     </v-card>
   </v-container>
 </template>
-
+<style scoped>
+  .v-form{
+    width:100%;
+  }
+</style>
 <script>
 import StorageService from "@/services/StorageService";
 
