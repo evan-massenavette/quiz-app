d442de0ce1f0b58b95e5f9680c3bc8d4a3a3a2fb
2K-d <rouxv894@gmail.com>
2023-01-06 09:29:54 +0100

    ajout requete backend backoffice

--- quiz-ui/src/services/QuizApiService.js
+++ quiz-ui/src/services/QuizApiService.js
@@ -27,6 +27,7 @@ export default {
         console.error(error);
       });
   },
+  // Front office
   getQuizInfo() {
     return this.call("get", "quiz-info");
   },
@@ -34,7 +35,27 @@ export default {
     return this.call("get", `questions?position=${position}`);
   },
   postScore(playerName, answers) {
-    const body = { playerName: playerName, answers: answers }
+    const body = { playerName, answers }
     return this.call("post", "participations", body)
+  },
+  // Back office
+  login(password) {
+    const body = { password }
+    return this.call("post", "login", body)
+  },
+  deleteAllQuestions(token) {
+    return this.call("delete", "questions/all", null, token)
+  },
+  deleteAllParticipations(token) {
+    return this.call("delete", "participations/all", null, token)
+  },
+  deleteQuestion(token, id) {
+    return this.call("delete", `questions/${id}`, null, token)
+  },
+  addQuestion(token, text, title, image, position, possibleAnswers) {
+    return this.call("post", "questions", { text, title, image, position, possibleAnswers }, token)
+  },
+  modifyQuestion(token, id, text, title, image, position, possibleAnswers) {
+    return this.call("put", `questions/${id}`, { text, title, image, position, possibleAnswers }, token)
   }
 };
