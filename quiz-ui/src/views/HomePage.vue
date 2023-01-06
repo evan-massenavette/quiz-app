3805e0cb228ac0f113b5893056b7acfe4812c762
Evan MASSENAVETTE <evan.massenavette@outlook.fr>
2023-01-06 18:08:57 +0100

    Simplified HomePage styling

--- quiz-ui/src/views/HomePage.vue
+++ quiz-ui/src/views/HomePage.vue
@@ -7,10 +7,11 @@ import ScoreTable from '@/views/ScoreTable.vue';
     <v-card id="main_card">
       <h1>Space quiz !</h1>
       <p>Here is a little quiz on space and astronomy.</p>
-      <v-btn id="goto_quiz_button" to="/start-new-quiz">
+      <v-btn to="/start-new-quiz" color="accent">
         Start the quiz now!
       </v-btn>
-      <v-container id="scores_container" v-if="true || registeredScores && registeredScores.length > 0">
+      <v-container id="scores_container" class="d-flex flex-column align-center"
+        v-if="true || registeredScores && registeredScores.length > 0">
         <h1>High scores</h1>
         <ScoreTable id="score_table" />
       </v-container>
@@ -29,55 +30,18 @@ export default {
     };
   },
   async created() {
+    // Get quioz info
     const response = await quizApiService.getQuizInfo();
     if (response === undefined) {
       console.error(`HomePage: Could not get quiz info`)
       return
     }
     this.registeredScores = response.data;
-
   }
 };
 </script>
 
 <style scoped>
-#main_card_wrapper {
-  display: flex;
-  justify-content: center;
-  width: 100%;
-}
-
-#main_card {
-  padding: 10px 20px;
-  width: 800px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: fit-content;
-}
-
-#main_card h1 {
-  text-align: center;
-}
-
-#main_card p {
-  margin-top: 10px;
-  margin-bottom: 10px;
-}
-
-#goto_quiz_button {
-  background-color: rgb(var(--v-theme-accent));
-  width: fit-content;
-}
-
-#scores_container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-}
-
 #score_table {
   width: max(250px, min(500px, 80%));
 }
